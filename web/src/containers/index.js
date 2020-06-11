046fab9fb08e3b49660d7c8d66132d0473fe36da
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-06-11 00:07:09 -0300

    [container] placeholder for product detail container

---
+++ web/src/containers/index.js
@@ -0,0 +1,11 @@
+import Catalog from "./Catalog";
+import ProductDetail from "./Catalog";
+// import Search from "./Search";
+// import ShoppingCart from "./ShoppingCart";
+
+export {
+  Catalog,
+  ProductDetail,
+  // Search,
+  // ShoppingCart,
+};
