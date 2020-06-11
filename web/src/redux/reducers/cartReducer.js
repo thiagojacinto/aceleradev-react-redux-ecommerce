d93a12749c57f4d61551ad765d3d133d4b297877
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-06-11 00:08:35 -0300

    [fix] state attr + this attr changes

--- web/src/redux/reducers/cartReducer.js
+++ web/src/redux/reducers/cartReducer.js
@@ -3,7 +3,6 @@ import ShoppingConstants from "../constants";
 const CartInitialState = {
   count: 0,
   items: [],
-  isEmpty: true,
   errorMessage: "",
 };
 
@@ -14,15 +13,14 @@ const cartReducer = (state = CartInitialState, action) => {
     case ShoppingConstants.ADD_PRODUCT_TO_CART:
       return {
         ...state,
-        isEmpty: false,
-        count: count++,
+        count: state.count + 1,
         items: payload.filter(({product}) => product),
       };
     case ShoppingConstants.REMOVE_PRODUCT_FROM_CART:
       return {
         ...state,
-        count: count--,
-        items: items.filter((item) => item.id !== payload.productId),
+        count: state.count - 1,
+        items: state.items.filter((item) => item.id !== payload.productId),
       };
     default:
       return state;
