8a639d92fa5e06c047a8c2649869544c79aba9ff
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-06-11 00:14:11 -0300

    [implem] redux changes to use store + catalog route logic

--- web/src/routes/Routes.js
+++ web/src/routes/Routes.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
-export const Routes = (props) => (
+import endpoints from "../utils/endpoints";
+import {
+  getProductList,
+  addProductToCart,
+  removeProductFromCart,
+} from "../redux/actions";
+import { Catalog } from "../containers";
+
+const { getProducts } = endpoints;
+
+export const Routes = () => {
+  const dispatch = useDispatch();
+  const { path, url } = useRouteMatch();
+
+  useEffect(() => {
+    axios(getProducts)
+      .then((data) => data.data)
+      .then((res) => dispatch(getProductList(res)));
+  }, [dispatch]);
+
+  const { cart, catalog } = useSelector((state) => state);
+
+  return (
     <Switch>
-      <Route path="/" />
-      <Route path="/search" />
-      <Route path="/cart" />
+      <Route exact path="/">
+        <Catalog products={catalog.catalog} url={url} />
+      </Route>
+
+      <Route path={`${path}/search`} />
+      <Route path={`${path}/:productId`} />
     </Switch>
-);
+  );
+};
 
 export default Routes;
