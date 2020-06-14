cd10e72ef2c58da9d1bf8033f2386d7a0b89debf
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-06-14 01:06:37 -0300

    [fix] changing key to unique `code_color` property

--- web/src/components/Products/ProductsList.js
+++ web/src/components/Products/ProductsList.js
@@ -6,21 +6,22 @@ const ProductsList = ({ products, url }) => {
   return (
     <section className="product__list">
       <div className="container">
-        {products && products.map((item) => {
-          return (
-            <ProductItem
-              key={item.style}
-              id={item.style}
-              url={url}
-              imageUrl={item.image}
-              description={item.name}
-              regularPrice={item.regular_price}
-              actualPrice={item.actual_price}
-              discountPercentile={item.discount_percentage}
-              installments={item.installments}
-            />
-          );
-        })}
+        {products &&
+          products.map((item) => {
+            return (
+              <ProductItem
+                key={item.code_color}
+                id={item.code_color}
+                url={url}
+                imageUrl={item.image}
+                description={item.name}
+                regularPrice={item.regular_price}
+                actualPrice={item.actual_price}
+                discountPercentile={item.discount_percentage}
+                installments={item.installments}
+              />
+            );
+          })}
       </div>
     </section>
   );
