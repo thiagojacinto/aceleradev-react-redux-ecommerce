647186d2b40b08add64b44f86e6c3ca5a3565162
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-06-14 01:05:41 -0300

    [refactor] using utils + conditionals with onSale props

--- web/src/components/Products/ProductItem.js
+++ web/src/components/Products/ProductItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./Product.css";
-import imagePlaceholder from '../../assets/img-item-placeholder.png';
+import { applyImagePlaceholderIfBlank } from "../../utils/placeholder";
 
 const ProductItem = ({
   imageUrl,
@@ -12,17 +12,18 @@ const ProductItem = ({
   discountPercentile,
   id,
   url,
+  onSale = false,
 }) => {
   const price = regularPrice === actualPrice ? regularPrice : actualPrice;
-  if (imageUrl === "") imageUrl = imagePlaceholder;
+  imageUrl = applyImagePlaceholderIfBlank(imageUrl);
   return (
     <article className="product__item">
       <figure className="product__item__figure">
         <img src={imageUrl} alt={`product id: ${id}`} />
 
-        {discountPercentile !== "" && (
+        {onSale && (
           <div className="discount">
-            <span>{`-${discountPercentile}%`}</span>
+            <span>{`-${discountPercentile}`}</span>
           </div>
         )}
 
@@ -30,15 +31,15 @@ const ProductItem = ({
       </figure>
 
       <Link to={`${url}/${id}`}>
-        <h5 className="product__item__description">{description}</h5>
+        <h3 className="product__item__description">{description}</h3>
       </Link>
 
       <div className="product__item__price">
-        {discountPercentile !== "" && (
-          <span className="regular__price">{regularPrice}</span>
+        {onSale && (
+          <span className="regular__price">{` De: ${regularPrice}, por apenas:`}</span>
         )}
 
-        <p className="final__price">{price}</p>
+        <h3 className="final__price">{price}</h3>
       </div>
     </article>
   );
