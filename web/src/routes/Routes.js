36c2ac5fd334c613ab36c2889dbd5c513847c55d
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-07-02 23:04:31 -0300

    [routes] removing axios fetching as server was taken down

--- web/src/routes/Routes.js
+++ web/src/routes/Routes.js
@@ -1,16 +1,16 @@
 import React, { useEffect } from "react";
 import { Route, Switch, useRouteMatch, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import axios from "axios";
-// import catalogMock from "../__test__/mockups/catalog.json";
+// import axios from "axios";
+import catalogMock from "../__test__/mockups/catalog.json";
 
-import endpoints from "../utils/endpoints";
-import { getProductList } from "../redux/actions";
+// import endpoints from "../utils/endpoints";
+import { setProductList } from "../redux/actions";
 import { Catalog, ProductDetail, ShoppingCart } from "../containers";
 import { Search } from "../containers";
 import { CartSection } from "../components/Cart";
 
-const { getProducts } = endpoints; // FETCH NECESSARY
+// const { getProducts } = endpoints; // FETCH NECESSARY
 
 const Routes = () => {
   const dispatch = useDispatch();
@@ -18,10 +18,10 @@ const Routes = () => {
   let location = useLocation();
 
   useEffect(() => {
-    axios(getProducts)
-      .then((data) => data.data)
-      .then((res) => dispatch(getProductList(res)));
-    // dispatch(getProductList(catalogMock));
+    // axios(getProducts)
+    //   .then((data) => data.data)
+    //   .then((res) => dispatch(getProductList(res)));
+    dispatch(setProductList(catalogMock));
   }, [dispatch]);
 
   const productDetail = `${path}product`;
